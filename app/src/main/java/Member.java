787e0fbc355259d5c7fd35b4a753154b0d1350c2
Thanchot <cryuboy@gmail.com>
2026-03-10 15:00:02 +0700

    first commit

---
+++ app/src/main/java/Member.java
@@ -0,0 +1,37 @@
+import java.io.Serializable;
+public class Member implements Serializable {
+    private String id;
+    private String names;
+    private String tel;
+    private String email;
+    public Member(String names, String tel, String email) {
+        this.id = id;
+        this.names = names;
+        this.tel = tel;
+        this.email = email;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNames() {
+        return names;
+    }
+    public void setNames(String names) {
+        this.names = names;
+    }
+    public String getTel() {
+        return tel;
+    }
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
